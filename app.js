7ed4b5772be4b9fffe678fe7ea5454791d47c635
Samrawit Seare <seare456.sam@gmail.com>
2026-05-02 15:17:11 +0300

    Add clock functionality to app.js
    Implement a clock that updates every second.

---
+++ app.js
@@ -0,0 +1,21 @@
+const clock = document.querySelector(".clock");
+
+function tick() {
+    const now = new Date();
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+    
+    // Use backticks (`) instead of single quotes (')
+    const html = `
+        <span>${h} :</span>
+        <span>${m} :</span>
+        <span>${s}</span>
+    `;
+    
+    clock.innerHTML = html;
+}
+tick();
+
+//update every second
+setInterval(tick, 1000);
